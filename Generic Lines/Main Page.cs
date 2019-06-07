928543394ecdb628700e880abd22faeb558c1f34
gordon2285 <g.crawford20@ntlworld.com>
2019-06-07 23:02:19 +0100

    tennis window updates

--- Generic Lines/Main Page.cs
+++ Generic Lines/Main Page.cs
@@ -75,98 +75,98 @@ namespace Generic_Lines
 
         private void Tennis_MGrass_P2_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
-            Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(550, 600);
-            Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(630, 700);
-            Tennis_Lines.ShowDialog();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
+            TennisLinesWindow.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(550, 600);
+            TennisLinesWindow.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(630, 700);
+            TennisLinesWindow.ShowDialog();
         }
 
         private void Tennis_MHard_P1_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
-            Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(650, 720);
-            Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(570, 620);
-            Tennis_Lines.ShowDialog();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
+            TennisLinesWindow.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(650, 720);
+            TennisLinesWindow.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(570, 620);
+            TennisLinesWindow.ShowDialog();
         }
 
         private void Tennis_MHard_Avg_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
-            Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(600, 660);
-            Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(600, 660);
-            Tennis_Lines.ShowDialog();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
+            TennisLinesWindow.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(600, 660);
+            TennisLinesWindow.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(600, 660);
+            TennisLinesWindow.ShowDialog();
         }
 
         private void Tennis_MHard_P2_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
-            Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(570, 620);
-            Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(650, 720);
-            Tennis_Lines.ShowDialog();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
+            TennisLinesWindow.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(570, 620);
+            TennisLinesWindow.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(650, 720);
+            TennisLinesWindow.ShowDialog();
         }
 
         private void Tennis_MClay_P1_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
-            Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(580, 630);
-            Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(500, 550);
-            Tennis_Lines.ShowDialog();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
+            TennisLinesWindow.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(580, 630);
+            TennisLinesWindow.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(500, 550);
+            TennisLinesWindow.ShowDialog();
         }
 
         private void Tennis_MClay_Avg_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
-            Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(550, 600);
-            Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(550, 600);
-            Tennis_Lines.ShowDialog();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
+            TennisLinesWindow.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(550, 600);
+            TennisLinesWindow.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(550, 600);
+            TennisLinesWindow.ShowDialog();
         }
 
         private void Tennis_MClay_P2_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
-            Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(500, 550);
-            Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(580, 630);
-            Tennis_Lines.ShowDialog();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
+            TennisLinesWindow.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(500, 550);
+            TennisLinesWindow.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(580, 630);
+            TennisLinesWindow.ShowDialog();
         }
 
         private void Tennis_WGrass_P1_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
-            Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(550, 600);
-            Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(480, 530);
-            Tennis_Lines.ShowDialog();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
+            TennisLinesWindow.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(550, 600);
+            TennisLinesWindow.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(480, 530);
+            TennisLinesWindow.ShowDialog();
         }
 
         private void Tennis_WGrass_Avg_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
-            Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(530, 580);
-            Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(530, 580);
-            Tennis_Lines.ShowDialog();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
+            TennisLinesWindow.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(530, 580);
+            TennisLinesWindow.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(530, 580);
+            TennisLinesWindow.ShowDialog();
         }
 
         private void Tennis_WGrass_P2_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
-            Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(480, 530);
-            Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(550, 600);
-            Tennis_Lines.ShowDialog();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
+            TennisLinesWindow.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(480, 530);
+            TennisLinesWindow.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(550, 600);
+            TennisLinesWindow.ShowDialog();
         }
 
         private void Tennis_WHard_P1_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
             Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(580, 630);
             Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(500, 550);
             Tennis_Lines.ShowDialog();
@@ -174,8 +174,8 @@ namespace Generic_Lines
 
         private void Tennis_WHard_Avg_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
             Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(550, 600);
             Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(550, 600);
             Tennis_Lines.ShowDialog();
@@ -183,8 +183,8 @@ namespace Generic_Lines
 
         private void Tennis_WHard_P2_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
             Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(500, 550);
             Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(580, 630);
             Tennis_Lines.ShowDialog();
@@ -192,8 +192,8 @@ namespace Generic_Lines
 
         private void Tennis_WClay_P1_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
             Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(530, 580);
             Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(450, 500);
             Tennis_Lines.ShowDialog();
@@ -201,8 +201,8 @@ namespace Generic_Lines
 
         private void Tennis_WClay_Avg_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
             Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(500, 550);
             Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(500, 550);
             Tennis_Lines.ShowDialog();
@@ -210,8 +210,8 @@ namespace Generic_Lines
 
         private void Tennis_WClay_P2_Click(object sender, EventArgs e)
         {
-            Tennis_Lines_Window Tennis_Lines;
-            Tennis_Lines = new Tennis_Lines_Window();
+            Tennis_Lines TennisLinesWindow;
+            TennisLinesWindow = new Tennis_Lines();
             Tennis_Lines.P1PercentageBox.Text = TennisLinesCalcs.ServePercentageP1(450, 500);
             Tennis_Lines.P2PercentageBox.Text = TennisLinesCalcs.ServePercentageP2(530, 580);
             Tennis_Lines.ShowDialog();
